52c3f337f8a720175eb02b8d1899892e6399c0bf
nayyaung9 <naygit.developer@gmail.com>
2019-09-01 13:32:54 +0630

    get access in api request

--- src/models/Search.js
+++ src/models/Search.js
@@ -1,3 +1,21 @@
 import { elements } from '../views/base'
 import { API_KEY } from './Api'
 import { WeatherData } from './weather-data'
+
+const updateWeather = weatherData => {
+  elements.weatherCity.textContent = weatherData.cityName;
+  elements.weatherDocs.textContent = weatherData.description;
+  elements.weatherTemp.textContent = weatherData.temperature;
+  elements.weatherBox.style.display = 'block';
+  elements.loadingText.style.display = 'none';
+};
+
+export const searchWeather = () => {
+  const CITY_NAME = elements.inputForm.value.trim()
+  if (CITY_NAME.length === 0) return alert('Please enter a city name')
+
+  elements.weatherBox.style.display = 'none';
+  elements.loadingText.style.display = 'block';
+
+  const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&appid=${API_KEY}`;
+}
--- src/views/base.js
+++ src/views/base.js
@@ -1,7 +1,8 @@
 export const elements = {
   body: document.querySelector('body'),
   inputForm: document.querySelector('.input-form'),
-  weather: document.querySelector('#weather'),
+  loadingText: document.querySelector('#loading'),
+  weatherBox: document.querySelector('#weather'),
   weatherCity: weather.firstElementChild,
   weatherDocs: document.querySelector('#weatherDescription'),
   weatherTemp: weather.lastElementChild
