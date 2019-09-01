32835a9d880f7c8dcfb0ea50bedafd70f5790250
nayyaung9 <naygit.developer@gmail.com>
2019-09-01 13:20:18 +0630

    Grab DOM in base.js

--- src/views/base.js
+++ src/views/base.js
@@ -1,5 +1,8 @@
 export const elements = {
   body: document.querySelector('body'),
-  container: document.querySelector('.container'),
-  inputForm: document.querySelector('.input-form')
+  inputForm: document.querySelector('.input-form'),
+  weather: document.querySelector('#weather'),
+  weatherCity: weather.firstElementChild,
+  weatherDocs: document.querySelector('#weatherDescription'),
+  weatherTemp: weather.lastElementChild
 }
