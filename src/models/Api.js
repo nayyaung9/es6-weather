970207e033811f9db5fde58874a941ef78ea23d0
nayyaung9 <naygit.developer@gmail.com>
2019-09-01 13:36:43 +0630

    create HTTP request

--- src/models/Api.js
+++ src/models/Api.js
@@ -1,1 +1,21 @@
 export const API_KEY = '1101c777c19f28cb85b1d7b77d88e6a6'
+
+export class Http {
+  static fetchData (url) {
+    return new Promise((resolve, reject) => {
+      const HTTP = new XMLHttpRequest();
+
+      HTTP.responseType = 'json';
+      HTTP.open('GET', url);
+      HTTP.send();
+
+      HTTP.onload = function () {
+        resolve(this.response);
+      }
+
+      HTTP.onerror = function () {
+        reject('Something went wrong...');
+      }
+    });
+  }
+}
