c1ce1da7a63d18be9925558722e9f4c8f3074158
nayyaung9 <naygit.developer@gmail.com>
2019-09-02 17:13:27 +0630

    render component in app.js

--- src/app.js
+++ src/app.js
@@ -8,11 +8,19 @@ import {
     themeUI
 } from './views/base'
 
+import * as homeView from './views/homeView'
+
 
 // rendering the whole UI
-elements.body.onload = () => searchWeather()
-// elements.location.reload(true) = () => reloadUI()
+// elements.body.onload = () => searchWeather()
+// elements.body.onload = () => homeView.renderHome()
+
 elements.addButton.addEventListener('click', createStore)
 elements.addItem.addEventListener('click', renderUI)
 elements.theme.addEventListener('click', themeUI)
 
+
+// On page load
+window.addEventListener('load', () => {
+    homeView.renderHome()
+})
