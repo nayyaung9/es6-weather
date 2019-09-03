6c6db6105c9993c609cfc4a1f998a286f0c80129
nayyaung9 <naygit.developer@gmail.com>
2019-09-03 22:13:24 +0630

    create Global State

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import { elements } from './views/base'
 
 import * as homeView from './views/homeView'
+import Current from './models/Current';
+
+// global App State
+const state = {};
 
 const currentController = () => {
+  
+  if(!state.current) state.current = new Current()
 
+  
 }
 
 
--- src/models/Current.js
+++ src/models/Current.js
@@ -1,5 +1,15 @@
 export default class Current {
   constructor() {
-    this.weather = []
+    this.weather = [],
+    this.coords = []
   }
+
+  async getCoords() {
+    window.navigator.geolocation.getCurrentPosition(getposition)
+
+    getposition = position => {
+      this.coords = [position.coords.latitude, position.coords.longitude]
+    }
+  }
+
 }
