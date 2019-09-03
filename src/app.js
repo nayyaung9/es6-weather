1d87a29c65959b465802d1d6883136e546e216df
nayyaung9 <naygit.developer@gmail.com>
2019-09-03 22:06:53 +0630

    create homeView

--- src/app.js
+++ src/app.js
@@ -3,15 +3,22 @@ import { createStore } from './models/Saved'
 import { 
     elements, 
     renderUI,
-    reloadUI,
     themeUI
 } from './views/base'
 
+const currentController = () => {
+
+}
+
 
 // rendering the whole UI
-elements.body.onload = () => searchWeather()
+// elements.body.onload = () => searchWeather()
 // elements.location.reload(true) = () => reloadUI()
 elements.addButton.addEventListener('click', createStore)
 elements.addItem.addEventListener('click', renderUI)
 elements.theme.addEventListener('click', themeUI)
 
+
+window.addEventListener('load', () => {
+  elements.body
+});
---
+++ src/models/Current.js
@@ -0,0 +1,5 @@
+export default class Current {
+  constructor() {
+    this.weather = []
+  }
+}
